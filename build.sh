9ed1957cc5c18ce73ef7e369d1c01486fc94644a
Joel Baranick <joel.baranick@ensighten.com>
2018-11-29 01:51:10 -0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,202 @@
+#!/usr/bin/env bash
+
+# Copyright © 2018 Joel Baranick <jbaranick@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+BUILD_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+BINARY_DIR="$BUILD_DIR/.bin"
+VERSION=$(cat $BUILD_DIR/.version)
+
+function verbose() { echo -e "$*"; }
+function error() { echo -e "ERROR: $*" 1>&2; }
+function fatal() { echo -e "ERROR: $*" 1>&2; exit 1; }
+function pushd () { command pushd "$@" > /dev/null; }
+function popd () { command popd > /dev/null; }
+
+function trap_add() {
+  localtrap_add_cmd=$1; shift || fatal "${FUNCNAME} usage error"
+  for trap_add_name in "$@"; do
+    trap -- "$(
+      extract_trap_cmd() { printf '%s\n' "$3"; }
+      eval "extract_trap_cmd $(trap -p "${trap_add_name}")"
+      printf '%s\n' "${trap_add_cmd}"
+    )" "${trap_add_name}" || fatal "unable to add to trap ${trap_add_name}"
+  done
+}
+declare -f -t trap_add
+
+function get_platform() {
+  unameOut="$(uname -s)"
+  case "${unameOut}" in
+    Linux*)
+      echo "linux"
+    ;;
+    Darwin*)
+      echo "darwin"
+    ;;
+    *)
+      echo "Unsupported machine type :${unameOut}"
+      exit 1
+    ;;
+  esac
+}
+
+PLATFORM=$(get_platform)
+GLIDE=$BINARY_DIR/glide
+GLIDE_URL="https://github.com/Masterminds/glide/releases/download/v0.13.1/glide-v0.13.1-$PLATFORM-amd64.tar.gz"
+GOX="gox"
+GOMETALINTER=$BINARY_DIR/gometalinter
+GOMETALINTER_URL="https://github.com/alecthomas/gometalinter/releases/download/v2.0.4/gometalinter-2.0.4-$PLATFORM-amd64.tar.gz"
+
+function download_glide() {
+  if [ ! -f "$GLIDE" ]; then
+    verbose "   --> $GLIDE"
+    local tmpdir=`mktemp -d`
+    trap_add "rm -rf $tmpdir" EXIT
+    pushd $tmpdir
+    curl -L -s -O $GLIDE_URL || fatal "failed to download 'GLIDE_URL': $?"
+    for i in *.tar.gz; do
+      [ "$i" = "*.tar.gz" ] && continue
+      tar xzf "$i" -C $tmpdir --strip-components 1 && rm -r "$i"
+    done
+    popd
+    mkdir -p $BINARY_DIR
+    cp $tmpdir/* $BINARY_DIR/
+  fi
+}
+
+function download_gometalinter() {
+  if [ ! -f "$GOMETALINTER" ]; then
+    verbose "   --> $GOMETALINTER"
+    local tmpdir=`mktemp -d`
+    trap_add "rm -rf $tmpdir" EXIT
+    pushd $tmpdir
+    curl -L -s -O $GOMETALINTER_URL || fatal "failed to download '$GOMETALINTER_URL': $?"
+    for i in *.tar.gz; do
+      [ "$i" = "*.tar.gz" ] && continue
+      tar xzf "$i" -C $tmpdir --strip-components 1 && rm -r "$i"
+    done
+    popd
+    mkdir -p $BINARY_DIR
+    cp $tmpdir/* $BINARY_DIR/
+  fi
+}
+
+function download_gox() {
+  if [ ! -x "$(command -v $GOX)" ]; then
+    echo "   --> $GOX"
+    go get github.com/mitchellh/gox || fatal "go get 'github.com/mitchellh/gox' failed: $?"
+  fi
+}
+
+function download_binaries() {
+  download_glide || fatal "failed to download 'glide': $?"
+  download_gox || fatal "failed to download 'gox': $?"
+  download_gometalinter || fatal "failed to download 'gometalinter': $?"
+  export PATH=$PATH:$BINARY_DIR
+}
+
+function run() {
+  local revision=`git rev-parse HEAD`
+  local branch=`git rev-parse --abbrev-ref HEAD`
+  local host=`hostname`
+  local buildDate=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+  go version | grep -q 'go version go1.11.2 ' || fatal "go version is not 1.11.2"
+
+  if [ -z "$TRAVIS" ]; then
+    verbose "Cleanup dist..."
+    rm -rf dist/*
+  fi
+
+  verbose "Fetching binaries..."
+  download_binaries
+
+  verbose "Getting dependencies..."
+  $GLIDE install -v || fatal "glide install failed: $?"
+
+  local gofiles=$(find . -path ./vendor -prune -o -print | grep '\.go$')
+
+  verbose "Installing dependencies..."
+  go install ./... || fatal "go install failed: $?"
+  go test -i ./... || fatal "go test install failed: $?"
+
+  verbose "Formatting source..."
+  if [[ ${#gofiles[@]} -gt 0 ]]; then
+    while read -r gofile; do
+      gofmt -s -w $PWD/$gofile
+    done <<< "$gofiles"
+  fi
+
+  if [ -n "$TRAVIS" ] && [ -n "$(git status --porcelain)" ]; then
+    fatal "Source not formatted"
+  fi
+
+  verbose "Linting source..."
+  $GOMETALINTER --disable-all --enable=vet --enable=gocyclo --cyclo-over=15 --enable=golint --min-confidence=.85 --enable=ineffassign --skip=Godeps --skip=vendor --skip=third_party --skip=testdata --vendor ./... || fatal "gometalinter failed: $?"
+
+  verbose "Checking licenses..."
+  licRes=$(
+  for file in $(find . -type f -iname '*.go' ! -path './vendor/*'); do
+    head -n3 "${file}" | grep -Eq "(Copyright|generated|GENERATED)" || error "  Missing license in: ${file}"
+  done;)
+  if [ -n "${licRes}" ]; then
+  	fatal "license header checking failed:\n${licRes}"
+  fi
+
+  verbose "Running tests..."
+  if [ -n "$TRAVIS" ]; then
+    if [ ! -x "$(command -v goveralls)" ]; then
+      echo "Getting goveralls..."
+      go get github.com/mattn/goveralls || fatal "go get 'github.com/mattn/goveralls' failed: $?"
+    fi
+    goveralls -v -service=travis-ci -ignore=main.go,testutil/server.go,testutil/golden.go || fatal "goveralls: $?"
+  else
+    go test -v ./... || fatal "$gopackage tests failed: $?"
+  fi
+
+  XC_ARCH=${XC_ARCH:-"386 amd64 arm arm64"}
+  XC_OS=${XC_OS:-"solaris darwin freebsd linux windows"}
+  if [ -z "$TRAVIS" ]; then
+    XC_OS=$(go env GOOS)
+    XC_ARCH=$(go env GOARCH)
+  fi
+
+  verbose "Building binaries..."
+  $GOX -os="${XC_OS}" -arch="${XC_ARCH}" -osarch="!darwin/arm !darwin/arm64" -ldflags "-X github.com/prometheus/common/version.Version=$VERSION -X github.com/prometheus/common/version.Revision=$revision -X github.com/prometheus/common/version.Branch=$branch -X github.com/prometheus/common/version.BuildUser=$USER@$host -X github.com/prometheus/common/version.BuildDate=$buildDate" -output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}"  || fatal "gox failed: $?"
+
+  if [ -n "$TRAVIS" ]; then
+    verbose "Creating archives..."
+    cd dist
+    set -x
+    for f in *; do
+      filename=$(basename "$f")
+      extension="${filename##*.}"
+      filename="${filename%.*}"
+      if [[ "$filename" != "$extension" ]] && [[ -n "$extension" ]]; then
+        extension=".$extension"
+      else
+        extension=""
+      fi
+      archivename="$filename.tar.gz"
+      verbose "   --> $archivename"
+      genericname="acurite_exporter$extension"
+      mv -f "$f" "$genericname"
+      tar -czf $archivename "$genericname"
+      rm -rf "$genericname"
+    done
+  fi
+}
+
+run "$@"
--- main.go
+++ main.go
@@ -1,1 +1,306 @@
-package acurite_exporter
+// Copyright © 2018 Joel Baranick <jbaranick@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"github.com/markphelps/optional"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/log"
+	"github.com/prometheus/common/version"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"net/http"
+	"net/http/httputil"
+	_ "net/http/pprof"
+	"net/url"
+	"strconv"
+	"sync"
+	"time"
+)
+
+const (
+	samplePath = "/weatherstation/updateweatherstation"
+)
+
+var (
+	listenAddress = kingpin.Flag("web.listen-address", "Address on which to expose metrics.").Default(":9519").String()
+	metricsPath   = kingpin.Flag("web.telemetry-path", "Path under which to expose Prometheus metrics.").Default("/metrics").String()
+	sampleExpiry  = kingpin.Flag("acurite.sample-expiry", "How long a sample is valid for.").Default("15m").Duration()
+
+	acuriteSampleMappings = []acuriteSampleMapping{
+		{"sensorbattery", "battery", "", float, prometheus.GaugeValue,
+			func(hub string, sensorType string, sensor string, name string, value string) optional.Float64 {
+				switch value {
+				case "low":
+					return optional.NewFloat64(0)
+				case "normal":
+					return optional.NewFloat64(1)
+				default:
+					log.Warnf("Unsupported %s value '%s' for %s sensor '%s' on hub '%s'", name, value, sensorType, sensor, hub)
+					return optional.Float64{}
+				}
+			}},
+		{"tempf", "temperature", "", float, prometheus.GaugeValue, mapFromFloat},
+		{"humidity", "humidity", "", integer, prometheus.GaugeValue, mapFromInteger},
+		{"dewptf", "dewpoint", "", float, prometheus.GaugeValue, mapFromFloat},
+	}
+
+	lastProcessed = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "acurite_last_processed_timestamp_seconds",
+			Help: "Unix timestamp of the last processed acurite metric.",
+		},
+	)
+	sampleExpiryMetric = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "acurite_sample_expiry_seconds",
+			Help: "How long in seconds a metric sample is valid for.",
+		},
+	)
+	sampleInvalidMetric = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "acurite_sample_invalid_total",
+			Help: "Total number of samples that were invalid.",
+		},
+		[]string{"hub", "type", "sensor"},
+	)
+	sampleInvalidValueMetric = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "acurite_sample_invalid_value_total",
+			Help: "Total number of sample values that were invalid.",
+		},
+		[]string{"hub", "type", "sensor", "name"},
+	)
+)
+
+type acuriteValueType int
+
+const (
+	_ acuriteValueType = iota
+	float
+	integer
+)
+
+type acuriteSampleMapping struct {
+	Source             string
+	Name               string
+	Help               string
+	ValueType          acuriteValueType
+	MetricType         prometheus.ValueType
+	ConversionFunction func(hub string, sensorType string, sensor string, name string, value string) optional.Float64
+}
+
+type acuriteSampleId struct {
+	Hub    string
+	Sensor string
+	Source string
+}
+
+type acuriteSample struct {
+	Id        acuriteSampleId
+	Name      string
+	Labels    map[string]string
+	Help      string
+	Value     float64
+	Type      prometheus.ValueType
+	Timestamp time.Time
+}
+
+type acuriteCollector struct {
+	samples map[acuriteSampleId]*acuriteSample
+	mu      *sync.Mutex
+	ch      chan *acuriteSample
+}
+
+func newAcuriteCollector() *acuriteCollector {
+	c := &acuriteCollector{
+		ch:      make(chan *acuriteSample, 0),
+		mu:      &sync.Mutex{},
+		samples: map[acuriteSampleId]*acuriteSample{},
+	}
+	go c.processSamples()
+	return c
+}
+
+func mapFromInteger(hub string, sensorType string, sensor string, name string, value string) optional.Float64 {
+	parsedValue, err := strconv.Atoi(value)
+	if err != nil {
+		return optional.Float64{}
+	}
+	return optional.NewFloat64(float64(parsedValue))
+}
+
+func mapFromFloat(hub string, sensorType string, sensor string, name string, value string) optional.Float64 {
+	parsedValue, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return optional.Float64{}
+	}
+	return optional.NewFloat64(parsedValue)
+}
+
+func (c *acuriteCollector) processSample(sample string, values url.Values) {
+	log.Debugf("Incoming sample: %s", sample)
+	hub := values.Get("id")
+	sensorType := values.Get("mt")
+	if sensorType == "5N1" || sensorType == "Atlas" || sensorType == "tower" || sensorType == "ProOut" || sensorType == "ProIn" {
+		sensor := values.Get("sensor")
+		dateutc := values.Get("dateutc")
+		timestamp, err := time.Parse("2006-01-02T15:04:05", dateutc)
+		if err != nil {
+			log.Errorf("Failed to convert dateutc '%s' for %s sensor '%s' on hub '%s' to a Time: %s",
+				dateutc, sensorType, sensor, hub, err)
+			sampleInvalidMetric.WithLabelValues(hub, sensorType, sensor).Inc()
+			return
+		}
+
+		labels := map[string]string{
+			"hub":    hub,
+			"type":   sensorType,
+			"sensor": sensor,
+		}
+		for _, acuriteSampleMapping := range acuriteSampleMappings {
+			rawValue := values.Get(acuriteSampleMapping.Source)
+			if rawValue != "" {
+				value := acuriteSampleMapping.ConversionFunction(hub, sensorType, sensor, acuriteSampleMapping.Source, rawValue)
+				if value.Present() {
+					value.If(func(f float64) {
+						sampleId := acuriteSampleId{
+							Hub:    hub,
+							Sensor: sensor,
+							Source: acuriteSampleMapping.Source,
+						}
+						sample := acuriteSample{
+							Id:        sampleId,
+							Name:      fmt.Sprintf("acurite_sensor_%s", acuriteSampleMapping.Name),
+							Value:     f,
+							Labels:    labels,
+							Type:      acuriteSampleMapping.MetricType,
+							Help:      acuriteSampleMapping.Help,
+							Timestamp: timestamp,
+						}
+						log.Debugf("Sample: %+v", sample)
+						lastProcessed.Set(float64(time.Now().UnixNano()) / 1e9)
+						c.ch <- &sample
+					})
+				} else {
+					log.Errorf("Failed to convert %s '%s' for %s sensor '%s' on hub '%s' to a %v: %s",
+						acuriteSampleMapping.Name, rawValue, sensorType, sensor, hub, acuriteSampleMapping.ValueType, err)
+					sampleInvalidValueMetric.WithLabelValues(hub, sensorType, sensor, acuriteSampleMapping.Name).Inc()
+				}
+			}
+		}
+	}
+}
+
+func (c *acuriteCollector) processSamples() {
+	ticker := time.NewTicker(time.Minute).C
+
+	for {
+		select {
+		case sample, ok := <-c.ch:
+			if sample == nil || ok != true {
+				return
+			}
+			c.mu.Lock()
+			c.samples[sample.Id] = sample
+			c.mu.Unlock()
+		case <-ticker:
+			// Garbage collect expired samples.
+			ageLimit := time.Now().Add(-*sampleExpiry)
+			c.mu.Lock()
+			for k, sample := range c.samples {
+				if ageLimit.After(sample.Timestamp) {
+					delete(c.samples, k)
+				}
+			}
+			c.mu.Unlock()
+		}
+	}
+}
+
+func (c acuriteCollector) Collect(ch chan<- prometheus.Metric) {
+	ch <- lastProcessed
+
+	c.mu.Lock()
+	samples := make([]*acuriteSample, 0, len(c.samples))
+	for _, sample := range c.samples {
+		samples = append(samples, sample)
+	}
+	c.mu.Unlock()
+
+	ageLimit := time.Now().Add(-*sampleExpiry)
+	for _, sample := range samples {
+		if ageLimit.After(sample.Timestamp) {
+			continue
+		}
+		ch <- prometheus.NewMetricWithTimestamp(sample.Timestamp,
+			prometheus.MustNewConstMetric(
+				prometheus.NewDesc(sample.Name, sample.Help, []string{}, sample.Labels),
+				sample.Type,
+				sample.Value,
+			),
+		)
+	}
+}
+
+func (c acuriteCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- lastProcessed.Desc()
+}
+
+func init() {
+	prometheus.MustRegister(version.NewCollector("acurite_exporter"))
+}
+
+func main() {
+	log.AddFlags(kingpin.CommandLine)
+	kingpin.Version(version.Print("acurite_exporter"))
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+
+	prometheus.MustRegister(sampleExpiryMetric)
+	sampleExpiryMetric.Set(sampleExpiry.Seconds())
+
+	prometheus.MustRegister(sampleInvalidMetric)
+
+	log.Infoln("Starting acurite_exporter", version.Info())
+	log.Infoln("Build context", version.BuildContext())
+
+	http.Handle(*metricsPath, promhttp.Handler())
+
+	upstreamUrl, _ := url.Parse("https://atlasapi.myacurite.com")
+	proxy := httputil.NewSingleHostReverseProxy(upstreamUrl)
+
+	c := newAcuriteCollector()
+	prometheus.MustRegister(c)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+      <head><title>Acurite Exporter</title></head>
+      <body>
+      <h1>Acurite Exporter</h1>
+      <p>Accepting Acurite samples at ` + samplePath + `</p>
+      <p><a href="` + *metricsPath + `">Metrics</a></p>
+      </body>
+      </html>`))
+	})
+	http.HandleFunc(samplePath, func(w http.ResponseWriter, r *http.Request) {
+		c.processSample(r.URL.RawQuery, r.URL.Query())
+		proxy.ServeHTTP(w, r)
+	})
+
+	log.Infoln("Listening on", *listenAddress)
+	log.Fatal(http.ListenAndServe(*listenAddress, nil))
+}
